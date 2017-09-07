133b833c2983e997698decc6131fac6e89e0a2ce
Alex Pham <alex.pham2010@gmail.com>
2017-09-07 11:37:06 +1000

    add new page

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ Rails.application.routes.draw do
 
    # Home controller and index page
   get 'home/index', to: 'home#index'
-  
+
+  get 'home/home', to: 'home#home'
+
   # set default root to Home controller and index page.
-  root to: 'home#index'
+  # root to: 'home#index'
+  root to: 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
