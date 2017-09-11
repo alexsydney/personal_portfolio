ba2dd19653e95672ec180db3f80932c513319c0a
Alex Pham <alex.pham2010@gmail.com>
2017-09-11 16:57:08 +1000

    Switch to port 465 for heroku mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
-    port:                 587, #465
+    port:                 465, #465
     domain:               "gmail.com",
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
